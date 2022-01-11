3ecae5c0e9c81747594d19e2c71c611d38cc7083
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2022-01-12 02:46:26 +0600

    done

---
+++ P1 Madlib.py
@@ -0,0 +1,12 @@
+#madlibs
+#string concatenation(how to put string togethere)
+
+
+adj =input("Adjective : ") #amazing
+verb1 =input("verb : ")  #sky drive
+verb2 =input("verb : ")   #Riyad
+famous_person =input("Famous person : ")
+
+madlib = f"computer programming is so {adj}! it makes me so execited all the time because \
+i love {verb1} . stay hydrated {verb2} like you are {famous_person}"
+print(madlib)
