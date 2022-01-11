b1bdd4f14df4a4b17e686f0646ba444b3a373337
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2022-01-12 02:21:30 +0600

    Create string-concatenation.py

---
+++ string-concatenation.py
@@ -0,0 +1,9 @@
+#madlibs
+#string concatenation(how to put string togethere)
+
+youtube = "2MinutesCoding"
+
+
+print("subscribe to "+youtube)
+print("subscribe to {}".format(youtube))
+print(f"subscribe to {youtube}")
